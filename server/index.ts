ea59253d22798acaa7ed9edb3b66c3e7c222d20d
youngvlyy <qqm2308@gmail.com>
2025-11-04 21:49:43 +0900

    20251104

--- server/index.ts
+++ server/index.ts
@@ -11,13 +11,13 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api", userRouter);
 app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.url);
   next();
 });
-app.use("/api", userRouter);
 
 
 // SPA 처리: React 빌드의 index.html을 반환
