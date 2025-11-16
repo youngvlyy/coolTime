122af4ed3d678b701240bd6229c0653d0250ab72
youngvlyy <qqm2308@gmail.com>
2025-11-16 22:37:29 +0900

    서버 빌드 수정

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import userRouter from "./routes/user";
+import userRouter from "./routes/user.js";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import { fileURLToPath } from "url";
 import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config();
 
