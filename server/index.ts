a9df8c736052391fb91b3e1c394c432b03932fb3
youngvlyy <qqm2308@gmail.com>
2025-11-16 23:28:23 +0900

    서버 빌드 수정

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import userRouter from "./routes/user.js";
+import userRouter from "./routes/user";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import { fileURLToPath } from "url";
 import path from "path";
 
 dotenv.config();
@@ -14,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", userRouter);
 app.use("/api/user", userRouter);
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.url);
@@ -23,9 +22,9 @@ app.use((req, res, next) => {
 
 
 // SPA 처리: React 빌드의 index.html을 반환
-app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
+// app.get(/.*/, (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+// });
 
 
 
