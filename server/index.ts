469121ff899a91463c9629ea5b80f32f291c9e55
youngvlyy <qqm2308@gmail.com>
2025-11-17 17:41:15 +0900

    서버 빌드 수정

--- server/index.ts
+++ server/index.ts
@@ -11,9 +11,10 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use("/api", userRouter);
 // app.use(express.static(path.join(__dirname, "../../client/dist")));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log("Incoming request:", req.method, req.url);
   next();
