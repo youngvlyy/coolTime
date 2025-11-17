9669dde2659856a74978db60565b9ab290ba0369
youngvlyy <qqm2308@gmail.com>
2025-11-17 17:59:32 +0900

    서버 빌드 수정

--- server/index.ts
+++ server/index.ts
@@ -42,4 +42,4 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`Server running on http://localhost:${PORT}`));
