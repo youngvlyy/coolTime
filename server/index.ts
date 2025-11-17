c5b1c9b2b43933d8b7607f8eb5f6a3bf2f015453
youngvlyy <qqm2308@gmail.com>
2025-11-17 17:04:36 +0900

    서버 빌드 수정

--- server/index.ts
+++ server/index.ts
@@ -12,7 +12,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", userRouter);
-app.use("/api/user", userRouter);
 // app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
