9abb6efc3b50c508644b59a01f91c5b2c1746ad3
youngvlyy <qqm2308@gmail.com>
2025-11-18 21:53:57 +0900

    에러 정상처리 해결

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -19,7 +19,7 @@ const Home: React.FC<HomeProps> = ({ user }) => {
   /** 유저 정보 로드 */
   useEffect(() => {
     axios.get(`/api/user/${user.uid}/${user.email}`).then((res) => {
-      setBody(res.data.body[0] || []);
+      setBody(res.data.body[0] || {});
       setFoods(res.data.food || []);
     });
   }, [user]);
